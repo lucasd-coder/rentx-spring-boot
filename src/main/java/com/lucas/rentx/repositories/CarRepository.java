707750b216b0e582bc97370f32aa344d3460de54
Lucas da Silva <lucasdasilva68@yahoo.com>
2021-08-27 11:42:33 -0300

    add querydsl, remove Specification carRepositorySpecification, remove metamodel Car_

--- src/main/java/com/lucas/rentx/repositories/CarRepository.java
+++ src/main/java/com/lucas/rentx/repositories/CarRepository.java
@@ -5,14 +5,14 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import com.lucas.rentx.entities.Car;
 
-public interface CarRepository extends JpaRepository<Car, UUID>, JpaSpecificationExecutor<Car>{
+public interface CarRepository extends JpaRepository<Car, UUID>, QuerydslPredicateExecutor<Car> {
 
 	Car findByLicensePlate(String licensePlate);
 
-	Page<Car> findByAvaiableTrueAndNameAndBrandAndCategoryId(String name, String brand,
-			UUID categoryId, Pageable pageable);
+	Page<Car> findByAvaiableTrueAndNameAndBrandAndCategoryId(String name, String brand, UUID categoryId,
+			Pageable pageable);
 }
--- src/main/java/com/lucas/rentx/services/CarService.java
+++ src/main/java/com/lucas/rentx/services/CarService.java
@@ -1,7 +1,5 @@
 package com.lucas.rentx.services;
 
-import static com.lucas.rentx.repositories.customization.CarRepositorySpecification.findByAvailableTrue;
-
 import java.util.Optional;
 import java.util.UUID;
 
@@ -14,8 +12,10 @@ import org.springframework.transaction.annotation.Transactional;
 import com.lucas.rentx.dto.CarDTO;
 import com.lucas.rentx.dto.CarResponseDTO;
 import com.lucas.rentx.entities.Car;
+import com.lucas.rentx.entities.QCar;
 import com.lucas.rentx.repositories.CarRepository;
 import com.lucas.rentx.services.exceptions.ObjectNotFoundException;
+import com.querydsl.core.BooleanBuilder;
 
 @Service
 public class CarService {
@@ -41,12 +41,29 @@ public class CarService {
 
 	@Transactional(readOnly = true)
 	public Page<CarResponseDTO> listAvailable(String name, String brand, UUID categoryId, Pageable pageable) {
-		Page<Car> list = carRepository.findAll(findByAvailableTrue(brand, name, categoryId), pageable);
+		QCar entity = QCar.car;
+		BooleanBuilder where = new BooleanBuilder();
+
+		where.and(entity.avaiable.eq(true));
+
+		if (brand != null) {
+			where.and(entity.brand.eq(brand));
+		}
+
+		if (name != null) {
+			where.and(entity.name.eq(name));
+		}
+
+		if (categoryId != null) {
+			where.and(entity.category.id.eq(categoryId));
+		}
+
+		Page<Car> list = carRepository.findAll(where, pageable);
 		Page<CarResponseDTO> listDto = list.map(obj -> new CarResponseDTO(obj));
 
 		return listDto;
 	}
-		
+
 	public Car fromDto(CarDTO objDto) {
 		return new Car(null, objDto.getName(), objDto.getDescription(), objDto.getDaily_rate(), true,
 				objDto.getLicense_plate(), objDto.getFine_amount(), objDto.getBrand(), null, null);
